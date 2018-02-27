3b16c59675a4318be1652da8a368da7007c1e4a1
alexis <lex.olivier20@gmail.com>
2018-02-27 06:16:40 -0600

    FEBRUARY 27th

--- ClassRoom/Circle.java
+++ ClassRoom/Circle.java
@@ -9,6 +9,15 @@ public class Circle{
   //properties
   private double radius;
 
+  //Constructor
+  public Circle(){
+
+  }
+  //Constructor Circle overload
+  public Circle(double radius){
+    this.radius = radius;
+  }
+
   //methods
   public void setRadius(Double radius){
     this.radius = radius;
@@ -20,11 +29,12 @@ public class Circle{
   public double getArea(){
     return Math.pow(this.radius, 2) *Math.PI;
   }
+    //Main Method
 public static void main (String[]args){
-  Circle circle = new Circle();
 
-  circle.setRadius(3.14);
+  double radius = Double.parseDouble(JOptionPane.showInputDialog("Enter the radius"));
+  Circle circle = new Circle(radius);
 
-JOptionPane.showMessageDialog(null, circle.getArea());
+  JOptionPane.showMessageDialog(null, "The circle´s area with a radius of "+ circle.getRadius() + " is "+ circle.getArea());
   }
 }
